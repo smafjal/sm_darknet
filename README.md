421dc404891f2dddbf7e0257746881cf383658a1
smafjal <afjal.sm19@gmail.com>
2017-02-09 22:20:14 +0600

    init all

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# sm_darknet
+![Darknet Logo](http://pjreddie.com/media/files/darknet-black-small.png)
+
+#Darknet#
+Darknet is an open source neural network framework written in C and CUDA. It is fast, easy to install, and supports CPU and GPU computation.
+
+For more information see the [Darknet project website](http://pjreddie.com/darknet).
+
+For questions or issues please use the [Google Group](https://groups.google.com/forum/#!forum/darknet).
+
+
+## My Added Docs -help
+
+#Img-Check
+./darknet imtest data/eagle.jpg
+
+# Detactor
+./darknet detect cfg/yolo.cfg yolo.weights data/dog.jpg
+./darknet detect cfg/yolo.cfg yolo.weights data/dog.jpg -thresh 0
+
+
---
+++ img_detection.sh
@@ -0,0 +1,16 @@
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata/SUST_Pessimist.jpg
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata/hacker1.jpg
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights out_crazy_cats
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata2/out_crazy_cats -thresh 0.5
+
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata2/CR7
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata2/VID_01
+
+# ./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata2/VID_01
+
+./darknet detect cfg/yolo.cfg pre-trained-model/yolo.weights mydata2/IMG
